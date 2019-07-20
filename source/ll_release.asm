5da178ec371bd41ef6a073d5962ecf34a527a178
Mateusz Stompór <mateusz.stompor@ibm.com>
2019-07-21 00:43:21 +0200

    Extend insert tests, improve release method

--- source/ll_release.asm
+++ source/ll_release.asm
@@ -3,8 +3,10 @@
 ;
 
 %include "source/list.asm"
-%include "source/memory_management.asm"
 %include "source/sizes.asm"
+%include "source/offsets.asm"
+%include "source/memory_management.asm"
+
 
 global LL_RELEASE
 section .text
@@ -12,7 +14,26 @@ section .text
 LL_RELEASE:
     ; address sits in rdi register
     sub rsp, 8
-    mov rdi, LL_LIST_SIZE
+    mov r9, rdi
+    mov rcx, [rdi]
+
+    deallocate_nodes:
+      cmp rcx, 0
+      je deallocate_list
+      mov rdx, rcx
+      add rdx, LL_NODE_NEXT_OFFSET
+
+      mov rdi, rdx
+      mov rsi, LL_NODE_SIZE
+      call ll_mem_release
+
+      mov rdx, [rdx]
+      mov rcx, rdx
+      jmp deallocate_nodes
+    deallocate_list:
+
+    mov rdi, r9
+    mov rsi, LL_LIST_SIZE
     call ll_mem_release
     add rsp, 8
     ret
--- tests/test_insert.h
+++ tests/test_insert.h
@@ -27,8 +27,8 @@ protected:
 
 
 TEST_F(InsertTestFixture, EmptyListInsertion) { ;
-    lli_begin(it, list);
     int val = 1;
+    lli_begin(it, list);
     ll_insert(list, it, (void *)&val);
     ll_list * l = (ll_list *)list;
     ASSERT_EQ(l->head, l->last);
@@ -40,8 +40,8 @@ TEST_F(InsertTestFixture, EmptyListInsertion) { ;
 }
 
 TEST_F(InsertTestFixture, OneNodeListInsertionAtTheBeginning) { ;
-    lli_begin(it, list);
     int val1 = 1, val2 = 2;
+    lli_begin(it, list);
     ll_insert(list, it, (void *)&val1);
     lli_begin(it, list);
     ll_insert(list, it, (void *)&val2);
@@ -53,4 +53,55 @@ TEST_F(InsertTestFixture, OneNodeListInsertionAtTheBeginning) { ;
     ASSERT_EQ(l->last->value, (void *)&val1);
     ASSERT_EQ(l->head->next, l->last);
     ASSERT_EQ(l->head->value, (void *)&val2);
+}
+
+TEST_F(InsertTestFixture, TwoNodesListInsertionAtTheBeginning) { ;
+    int val1 = 1, val2 = 2, val3 = 3;
+    lli_begin(it, list);
+    ll_insert(list, it, (void *)&val1);
+    lli_begin(it, list);
+    ll_insert(list, it, (void *)&val2);
+    lli_begin(it, list);
+    ll_insert(list, it, (void *)&val3);
+
+    ll_list * l = (ll_list *)list;
+    ASSERT_EQ(l->size, 3);
+    ASSERT_NE(l->head, l->last);
+    ASSERT_EQ(l->last->next, (void *)NULL);
+    ASSERT_EQ(l->last->value, (void *)&val1);
+    ASSERT_EQ(l->head->next->next, l->last);
+    ASSERT_EQ(l->head->next->value, (void *)&val2);
+    ASSERT_EQ(l->head->value, (void *)&val3);
+    ASSERT_EQ(l->head->next->next->next, (void *)NULL);
+    ASSERT_EQ(l->head->next->next->value, (void *)&val1);
+}
+
+TEST_F(InsertTestFixture, EmptyListInsertAtTheEnd) { ;
+    int val = 1;
+    lli_end(it, list);
+    ll_insert(list, it, (void *)&val);
+
+    ll_list * l = (ll_list *)list;
+    ASSERT_EQ(l->head, l->last);
+    ASSERT_NE(l->head, (ll_node *)NULL);
+    ASSERT_EQ(l->size, 1);
+    ll_node * n = l->head;
+    ASSERT_EQ(n->value, &val);
+    ASSERT_EQ(n->next, (ll_node *)NULL);
+}
+
+TEST_F(InsertTestFixture, OneNodeListInsertAtTheEnd) { ;
+  int val1 = 1, val2 = 2;
+  lli_end(it, list);
+  ll_insert(list, it, (void *)&val1);
+  lli_end(it, list);
+  ll_insert(list, it, (void *)&val2);
+
+  ll_list * l = (ll_list *)list;
+  ASSERT_EQ(l->size, 2);
+  ASSERT_NE(l->head, l->last);
+  ASSERT_EQ(l->last->next, (void *)NULL);
+  ASSERT_EQ(l->last->value, (void *)&val2);
+  ASSERT_EQ(l->head->next, l->last);
+  ASSERT_EQ(l->head->value, (void *)&val1);
 }
