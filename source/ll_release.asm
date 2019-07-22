cd80f6e70b7da96fdbcf61b9ecc3ac1ca5ebb40c
Mateusz Stompór <mateusz.stompor@ibm.com>
2019-07-22 23:14:54 +0200

    Improve list deallocation

--- source/ll_release.asm
+++ source/ll_release.asm
@@ -5,6 +5,7 @@
 %include "source/list.asm"
 %include "source/sizes.asm"
 %include "source/offsets.asm"
+%include "source/constants.asm"
 %include "source/memory_management.asm"
 
 
@@ -13,27 +14,20 @@ section .text
 
 LL_RELEASE:
     ; address sits in rdi register
-    sub rsp, 8
-    mov r9, rdi
-    mov rcx, [rdi]
-
-    deallocate_nodes:
-      cmp rcx, 0
-      je deallocate_list
-      mov rdx, rcx
-      add rdx, LL_NODE_NEXT_OFFSET
-
-      mov rdi, rdx
-      mov rsi, LL_NODE_SIZE
-      call ll_mem_release
-
-      mov rdx, [rdx]
-      mov rcx, rdx
-      jmp deallocate_nodes
-    deallocate_list:
-
-    mov rdi, r9
+    push rbx
+    ; deallocate list
+    mov rbx, [rdi]
     mov rsi, LL_LIST_SIZE
     call ll_mem_release
-    add rsp, 8
+    ; deallocate nodes in the list if exist
+    deallocate_nodes:
+      cmp rbx, NULL_PTR
+      je finish
+      mov rdi, rbx
+      mov rbx, [rbx + 8]
+      mov rsi, LL_NODE_SIZE
+      call ll_mem_release
+      jmp deallocate_nodes
+    finish:
+    pop rbx
     ret
